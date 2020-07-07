fe3d3535cff907c437ee912aead3fe46cffd1367
Furkan Aksoy <aksoyfurkan45@gmail.com>
2020-07-07 17:51:06 +0300

    fix

--- this-game.lisp
+++ this-game.lisp
@@ -4,14 +4,6 @@
 
 ;;; THIS GAME
 
-;; Game loop needs to be designed. 
-;; a loop that jumps between states and when in jump raises event
-;; Of course there should be pre defined knowledge, Maybe in another file
-;; Monsters especialy. But their design requires prior knowledge of
-;; db and event system. Or not they can be loosly dependent
-;; loop might handle it. 
-;; Maybe first of all I should design the loop for sommething else
-;; 
 
 ;; testing
 (defparameter *agent-position* (cons 0 0))
